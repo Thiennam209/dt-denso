e1a3e13ce61911c6b878371bd2433de0469127f8
DESKTOP-8AKC1TB\Nam <thiennam209@gmail.com>
2023-11-10 09:09:10 +0700

    new function

--- functions/SignalRFunctions.cs
+++ functions/SignalRFunctions.cs
@@ -94,75 +94,12 @@ namespace SignalRFunctions
                         {
                             Alert = token["value"].ToObject<bool>();
                         }
-                        else
-                        {
-                            Time = token["value"].ToObject<string>();
-                            Part = token["value"].ToObject<int>();
-                            Station = token["value"].ToObject<string>();
-                            Serial = token["value"].ToObject<int>();
-                            AdjJudge = token["value"].ToObject<string>();
-                            Pressure = token["value"].ToObject<double>();
-                            IP1 = token["value"].ToObject<double>();
-                            CrimpJudge = token["value"].ToObject<string>();
-                            PerformJudge = token["value"].ToObject<string>();
-                            I1 = token["value"].ToObject<double>();
-                            I2 = token["value"].ToObject<double>();
-                            I3 = token["value"].ToObject<double>();
-                            I4 = token["value"].ToObject<double>();
-                            I5 = token["value"].ToObject<double>();
-                            I1I15 = token["value"].ToObject<double>();
-                            I2I14 = token["value"].ToObject<double>();
-                            I3I13 = token["value"].ToObject<double>();
-                            I4I12 = token["value"].ToObject<double>();
-                            Stick1 = token["value"].ToObject<double>();
-                            Stick3 = token["value"].ToObject<double>();
-                            Stick4 = token["value"].ToObject<double>();
-                            Flow = token["value"].ToObject<double>();
-                            Resp1_T1 = token["value"].ToObject<double>();
-                            Resp1_T2 = token["value"].ToObject<double>();
-                            Resp1_P5 = token["value"].ToObject<double>();
-                            Resp1_P6 = token["value"].ToObject<double>();
-                            Resp2_T1 = token["value"].ToObject<double>();
-                            Resp2_T2 = token["value"].ToObject<double>();
-                            Resp2_P5 = token["value"].ToObject<double>();
-                            Resp2_P6 = token["value"].ToObject<double>();
-                        }
                     }
 
                     log.LogInformation($"setting Alert to: {Alert}");
                     var property = new Dictionary<object, object>
                     {
                         {"MachineId", MachineId },
-                        {"Time",Time},
-                        {"Part",Part},
-                        {"Station",Station},
-                        {"Serial",Serial},
-                        {"AdjJudge",AdjJudge},
-                        {"Pressure",Pressure},
-                        {"IP1",IP1},
-                        {"CrimpJudge",CrimpJudge},
-                        {"PerformJudge",PerformJudge},
-                        {"I1",I1},
-                        {"I2",I2},
-                        {"I3",I3},
-                        {"I4",I4},
-                        {"I5",I5},
-                        {"I1I15",I1I15},
-                        {"I2I14",I2I14},
-                        {"I3I13",I3I13},
-                        {"I4I12",I4I12},
-                        {"Stick1",Stick1},
-                        {"Stick3",Stick3},
-                        {"Stick4",Stick4},
-                        {"Flow",Flow},
-                        {"Resp1_T1",Resp1_T1},
-                        {"Resp1_T2",Resp1_T2},
-                        {"Resp1_P5",Resp1_P5},
-                        {"Resp1_P6",Resp1_P6},
-                        {"Resp2_T1",Resp2_T1},
-                        {"Resp2_T2",Resp2_T2},
-                        {"Resp2_P5",Resp2_P5},
-                        {"Resp2_P6",Resp2_P6},
                         {"Alert",Alert}
                     };
                     return signalRMessages.AddAsync(
